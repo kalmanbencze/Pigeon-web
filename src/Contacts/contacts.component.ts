6ad54d371e80e1273e8366738ab158cd60b45d46
kalman.bencze <kalmanbncz@gmail.com>
2018-01-03 10:25:40 +0200

    deleted file

---
+++ src/Contacts/contacts.component.ts
@@ -0,0 +1,20 @@
+import {Component} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {ContactsService} from '../services/contacts.service';
+
+@Component({
+  selector: 'messages-component',
+  templateUrl: 'contacts.component.html',
+  styleUrls: ['contacts.component.css']
+})
+export class ContactsComponent {
+  title = 'Contacts';
+  contacts$: Observable<Object>;
+
+  constructor(private contactsService: ContactsService) {
+  }
+
+  fetchMessages(): void {
+    this.contacts$ = this.contactsService.getContacts();
+  }
+}
